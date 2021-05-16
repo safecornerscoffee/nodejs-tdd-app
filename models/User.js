4aae516e0d4cd204c4eb659b0a10048678550237
safecorners <safeconers.mobile@gmail.com>
2021-05-16 21:11:57 +0900

    define UserSchema

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    token: {
+        type: String,
+    },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.export = User;
